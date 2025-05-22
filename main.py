2a983f2432a8ee47e2f989b33a3b5e71c4515829
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-05-21 20:44:09 -0500

    refactor(main): output password dets differently
    Made get_password_details output the details in a list form.
    The code recieves it as three distinc values now.

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+import random
+
 def opening():
     print("Welcome to Portunus: the Ultimate Password Generator!")
 
 def get_password_details():
-    letter_count =  int(input("How many letters would you like in your password?\n> "))
-    symbol_count =  int(input("How many symbols would you like?\n> "))
-    number_count =  int(input("Finally, how many numbers?\n> "))
-    return {
-        "letters": letter_count, 
-        "symbols": symbol_count,
-        "numbers": number_count,
-        }
+    letter_cnt =  int(input("How many letters would you like in your password?\n> "))
+    symbol_cnt =  int(input("How many symbols would you like?\n> "))
+    number_cnt =  int(input("Finally, how many numbers?\n> "))
+    return [letter_cnt, symbol_cnt, number_cnt]
+
+def construct_password(letter_cnt: int, symbol_cnt: int, number_cnt: int):
+    alphabet = "abcdefjhijklmnopqrstuvwxyz"
+    numbers = "1234567890"
+    symbols = "!#$%&()*+"
+
+
 
 def main():
     opening()
-    password_details = get_password_details()
-    print(password_details)
+    letter_cnt, symbol_cnt, number_cnt = get_password_details()
+    # password = construct_password()
+    print(letter_cnt, symbol_cnt, number_cnt)
 
 if __name__ == "__main__":
     main()
