904b490d0551304893829dcccdb11ed249dfcf27
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-05-21 20:37:07 -0500

    feat(main): get password details
    - Add get_password_details function which gets and returns:
        - # of letters
        - # of symbols
        - # of numbers

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 def opening():
     print("Welcome to Portunus: the Ultimate Password Generator!")
-    return int(input("How many letters would you like in your password?\n> "))
+
+def get_password_details():
+    letter_count =  int(input("How many letters would you like in your password?\n> "))
+    symbol_count =  int(input("How many symbols would you like?\n> "))
+    number_count =  int(input("Finally, how many numbers?\n> "))
+    return {
+        "letters": letter_count, 
+        "symbols": symbol_count,
+        "numbers": number_count,
+        }
 
 def main():
-    letter_count = opening()
-    print(letter_count)
+    opening()
+    password_details = get_password_details()
+    print(password_details)
 
 if __name__ == "__main__":
     main()
