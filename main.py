b7ed9a20cb4d0c5499477ef5e3c0ca8fd71a70f0
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-05-21 20:52:01 -0500

    feat(main): construct and return random password
    Added the construct_password function which generates a password
    using the user-specified number of letters, symbols, and numbers.

--- main.py
+++ main.py
@@ -10,17 +10,30 @@ def get_password_details():
     return [letter_cnt, symbol_cnt, number_cnt]
 
 def construct_password(letter_cnt: int, symbol_cnt: int, number_cnt: int):
-    alphabet = "abcdefjhijklmnopqrstuvwxyz"
-    numbers = "1234567890"
+    alphabet = "abcdefjhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     symbols = "!#$%&()*+"
+    numbers = "1234567890"
+    
+    password = set()
 
+    while len(password) < letter_cnt:
+        password.add(random.choice(alphabet))
+    
+    while len(password) < letter_cnt + symbol_cnt:
+        password.add(random.choice(symbols))
 
+    while len(password) < letter_cnt + symbol_cnt + number_cnt:
+        password.add(random.choice(numbers))
+    
+    return "".join(password)
 
 def main():
     opening()
     letter_cnt, symbol_cnt, number_cnt = get_password_details()
     # password = construct_password()
     print(letter_cnt, symbol_cnt, number_cnt)
+    password = construct_password(letter_cnt, symbol_cnt, number_cnt)
+    print(password)
 
 if __name__ == "__main__":
     main()
